8aee22a384e3049a3cb6ccfaa35645b61d838b2a
SwarajKhadse <khadseswaraj@gmail.com>
2023-02-03 15:20:11 +0530

    ui fixes

--- src/App.js
+++ src/App.js
@@ -43,92 +43,20 @@ function App() {
   }
   return (
     <>
-      {data.map((e) => {
-       
-        final = number + 3;
-        if (e.id > number && e.id <= final) {
-          return (
-            <div className="main_card">
-              <div className="card">
-                <p>{e.username}</p>
-                <div>
-                  <h5>NAME</h5>
-                  <p className="card_detail">{e.name}</p>
-                </div>
-                <div>
-                  <h5>EMAIL</h5>
-                  <p className="card_detail">{e.email}</p>
-                </div>
-                <div>
-                  <h5>CITY</h5>
-                  <p className="card_detail">{e.address.city}</p>
-                </div>
-                <button onClick={setDis}>view details</button>
-              </div>
-              {/* {
-                (() => {
-                    if(e.id==display) {
-                            return (
-                                <p>Hi</p>
-                            )
-                        } 
-                })()  
-            }   */}
+        {data.map((e) => {
 
-              <div className="details" ref={inputElem}>
-                <h4 className="details_text">Description</h4>
-                <p className="details_text detail_para">
-                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                  Placeat distinctio dolorem ab dolores cumque sint aliquid
-                  dolor itaque, ut sunt assumenda dicta nam repellendus ratione
-                  illum sed minus maiores labore?
-                </p>
-                <div className="col">
-                  <div className="col1">
-                    <h4 className="details_text">Contact Person</h4>
-                    <p className="details_text detail_para">{e.name}</p>
-                    <h4 className="details_text">Phone</h4>
-                    <p className="details_text detail_para detail_extra">
-                      {e.phone}
-                    </p>
-                    <h4 className="details_text">Website</h4>
-                    <p className="details_text detail_para detail_extra">
-                      {e.website}
-                    </p>
-                    <h4 className="details_text">Company Name</h4>
-                    <p className="details_text detail_para detail_extra">
-                      {e.company.name}
-                    </p>
-                  </div>
-                  <div className="col2">
-                    <h4 className="details_text">Address</h4>
-                    <p className="details_text detail_para">
-                      {e.address.street}
-                    </p>
-                    <h4 className="details_text">Suite</h4>
-                    <p className="details_text detail_para detail_extra">
-                      {e.address.suite}
-                    </p>
-                    <h4 className="details_text">City</h4>
-                    <p className="details_text detail_para detail_extra">
-                      {e.address.city}
-                    </p>
-                    <h4 className="details_text">Zipcode</h4>
-                    <p className="details_text detail_para detail_extra">
-                      {e.address.zipcode}
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          );
-        }
-      })}
-      <div className="butt">
-        <button onClick={moveLeft}> Left</button>
-        <button onClick={moveRight}>Right</button>
-      </div>
+            final = number + 3;
+            if (e.id > number && e.id <= final) {
+                return (
+                    <Card e={e} />
+                );
+            }
+        })}
+        <div className="butt">
+            <button onClick={moveLeft}> Left</button>
+            <button onClick={moveRight}>Right</button>
+        </div>
     </>
-  );
+);
 }
 export default App;
--- src/Card.js
+++ src/Card.js
@@ -1,13 +1,103 @@
-import React from 'react'
+import React, { useState ,useRef } from 'react'
 import './Card.css'
-function Card(props) {
-    
-  return (
-    <>
+export default function Card({ e }) {
+    const [showDetails, setShowDetails] = useState(false);
+    var butt = "Show Details";
+    if(showDetails==false)
+    {
+        butt="Show Details";
+    }
+    else{
+        butt = "Hide Details"
+    }
+    const inputElem = useRef(1);
+    function setDis(  ){
+        alert("dshf ")
+        setShowDetails(true)
        
-    
-    </>
-  )
+           if(showDetails==false)
+            {
+
+                inputElem.current.style.display="block"
+            }
+            else{
+                inputElem.current.style.display="none"
+                butt = "show Details"
+            }
+      }
+    return (
+        <div>
+
+            <div className="main_card">
+                <div className="card">
+                    <p>{e.username}</p>
+                    <div>
+                        <h5>NAME</h5>
+                        <p className="card_detail">{e.name}</p>
+                    </div>
+                    <div>
+                        <h5>EMAIL</h5>
+                        <p className="card_detail">{e.email}</p>
+                    </div>
+                    <div>
+                        <h5>CITY</h5>
+                        <p className="card_detail">{e.address.city}</p>
+                    </div>
+                    <button  onClick={setDis}>
+
+                           {butt}
+                    </button>
+                </div>
+
+                {<div className="details" ref={inputElem}>
+                    <h4 className="details_text">Description</h4>
+                    <p className="details_text detail_para">
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                        Placeat distinctio dolorem ab dolores cumque sint aliquid
+                        dolor itaque, ut sunt assumenda dicta nam repellendus ratione
+                        illum sed minus maiores labore?
+                    </p>
+                    <div className="col">
+                        <div className="col1">
+                            <h4 className="details_text">Contact Person</h4>
+                            <p className="details_text detail_para">{e.name}</p>
+                            <h4 className="details_text">Phone</h4>
+                            <p className="details_text detail_para detail_extra">
+                                {e.phone}
+                            </p>
+                            <h4 className="details_text">Website</h4>
+                            <p className="details_text detail_para detail_extra">
+                                {e.website}
+                            </p>
+                            <h4 className="details_text">Company Name</h4>
+                            <p className="details_text detail_para detail_extra">
+                                {e.company.name}
+                            </p>
+                        </div>
+                        <div className="col2">
+                            <h4 className="details_text">Address</h4>
+                            <p className="details_text detail_para">
+                                {e.address.street}
+                            </p>
+                            <h4 className="details_text">Suite</h4>
+                            <p className="details_text detail_para detail_extra">
+                                {e.address.suite}
+                            </p>
+                            <h4 className="details_text">City</h4>
+                            <p className="details_text detail_para detail_extra">
+                                {e.address.city}
+                            </p>
+                            <h4 className="details_text">Zipcode</h4>
+                            <p className="details_text detail_para detail_extra">
+                                {e.address.zipcode}
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                }
+           
+            </div>
+        </div>
+    )
 }
 
-export default Card
